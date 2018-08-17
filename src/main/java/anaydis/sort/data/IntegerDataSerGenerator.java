fb5d8cd1fc7ae475a8e6d188345c40dc5ccaf26e
matiasmiodosky <matias.miodosky@ing.austral.edu.ar>
2018-08-17 12:45:24 -0300

    Implemented IntegerDataSetGenerator

---
+++ src/main/java/anaydis/sort/data/IntegerDataSerGenerator.java
@@ -0,0 +1,48 @@
+package anaydis.sort.data;
+
+import org.jetbrains.annotations.NotNull;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+public class IntegerDataSerGenerator implements DataSetGenerator<Integer> {
+
+    private static final int RANDOM_LIMIT = 100;
+    @NotNull
+    @Override
+    public List<Integer> createAscending(int length) {
+        if (length <= 0) throw new IllegalArgumentException("Invalid length for data set: " + length);
+        List<Integer> integerList = new ArrayList<>();
+        for (int i = 0; i < length; i++) {
+            integerList.add(i);
+        }
+        return integerList;
+    }
+
+    @NotNull
+    @Override
+    public List<Integer> createDescending(int length) {
+        if (length <= 0) throw new IllegalArgumentException("Invalid length for data set: " + length);
+        List<Integer> integerList = new ArrayList<>();
+        for (int i = length; i > 0; i--) {
+            integerList.add(i);
+        }
+        return integerList;
+    }
+
+    @NotNull
+    @Override
+    public List<Integer> createRandom(int length) {
+        if (length <= 0) throw new IllegalArgumentException("Invalid length for data set: " + length);
+        List<Integer> integerList = new ArrayList<>();
+        for (int i = length; i > 0; i--) {
+            integerList.add((int) (Math.random() * RANDOM_LIMIT));
+        }
+        return integerList;
+    }
+
+    @NotNull
+    @Override
+    public Comparator<Integer> getComparator() {
+        return Integer::compareTo;
+    }
+}
