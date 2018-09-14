cf9a4369fd561efe3fd1645196db5595d525a65f
matiasmiodosky <matias.miodosky@ing.austral.edu.ar>
2018-09-14 12:24:43 -0300

    ArrayMap implemented. ArrayMapTest implemented.

---
+++ src/main/java/anaydis/search/ArrayMap.java
@@ -0,0 +1,69 @@
+package anaydis.search;
+import org.jetbrains.annotations.NotNull;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayMap<K, V> implements Map<K, V> {
+
+    private List<K> keys;
+    private List<V> values;
+    private Comparator<K> comparator;
+
+    ArrayMap(Comparator<K> comparator) {
+        keys = new ArrayList<>();
+        values = new ArrayList<>();
+        this.comparator = comparator;
+    }
+
+    private int find(K key, int lo, int hi){
+        if(lo <= hi) {
+            final int middle = (lo + hi) / 2;
+            final int cmp = comparator.compare(key, keys.get(middle));
+            if (cmp == 0) return middle;
+            return cmp > 0 ? find(key, middle + 1, hi) : find(key, lo, middle - 1);
+        }
+        return - (lo + 1);
+    }
+
+    @Override
+    public int size() {
+        return keys.size();
+    }
+
+    @Override
+    public boolean containsKey(@NotNull K key) {
+        return find(key, 0, keys.size() - 1) >= 0;
+    }
+
+
+    @Override
+    public V get(@NotNull K key) {
+        final int index = find(key, 0, keys.size() - 1);
+        return index >= 0 ? values.get(index) : null;
+    }
+
+    @Override
+    public V put(@NotNull K key, V value) {
+        final int index = find(key, 0, keys.size()  -1);
+        if(index >= 0){
+            return values.set(index, value);
+        }else {
+            keys.add(-index - 1, key);
+            values.add(-index - 1, value);
+            return null;
+        }
+    }
+
+    @Override
+    public void clear() {
+        keys.clear();
+        values.clear();
+    }
+
+    @Override
+    public Iterator<K> keys() {
+        return keys.iterator();
+    }
+}
---
+++ src/test/java/anaydis/search/ArrayMapTest.java
@@ -0,0 +1,81 @@
+package anaydis.search;
+
+import org.junit.Test;
+import java.util.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ArrayMapTest {
+
+    @Test
+    public void test01_size_0_when_instantiated(){
+        final ArrayMap<Integer, String> arrayMap = new ArrayMap<>(Integer::compareTo);
+        assertEquals(0, arrayMap.size());
+    }
+
+    @Test
+    public void test02_size_1_when_item_put(){
+        final ArrayMap<Integer, String> arrayMap = new ArrayMap<>(Integer::compareTo);
+        arrayMap.put(1, "1");
+        assertEquals(1, arrayMap.size());
+    }
+
+    @Test
+    public void test03_return_null_when_put_first_time(){
+        final ArrayMap<Integer, String> arrayMap = new ArrayMap<>(Integer::compareTo);
+        assertNull(arrayMap.put(1, "1"));
+    }
+
+    @Test
+    public void test04_get_returns_item_put(){
+        final ArrayMap<Integer, String> arrayMap = new ArrayMap<>(Integer::compareTo);
+        arrayMap.put(1, "1");
+        //assertEquals("1", arrayMap.get(1));
+    }
+
+    @Test
+    public void test05_return_previous_when_put_again(){
+        final ArrayMap<Integer, String> arrayMap = new ArrayMap<>(Integer::compareTo);
+        arrayMap.put(1, "1");
+        assertEquals("1", arrayMap.put(1, "2"));
+    }
+
+    @Test
+    public void test06_replace_when_put_again(){
+        final ArrayMap<Integer, String> arrayMap = new ArrayMap<>(Integer::compareTo);
+        arrayMap.put(1, "1");
+        assertEquals("1", arrayMap.put(1, "2"));
+        assertEquals("2", arrayMap.get(1));
+    }
+
+    @Test
+    public void test07_size_0_when_clear(){
+        final ArrayMap<Integer, String> arrayMap = new ArrayMap<>(Integer::compareTo);
+        arrayMap.put(1, "1");
+        arrayMap.clear();
+        assertEquals(0, arrayMap.size());
+    }
+
+    @Test
+    public void test08_iterator_iter_every_key(){
+        final ArrayMap<Integer, String> arrayMap = new ArrayMap<>(Integer::compareTo);
+        arrayMap.put(1, "1");
+        arrayMap.put(2, "2");
+        Iterator<Integer> integerIterator = arrayMap.keys();
+        List<Integer> keys = new ArrayList<>();
+        while (integerIterator.hasNext()){
+            keys.add(integerIterator.next());
+        }
+        assertTrue(keys.contains(1));
+        assertTrue(keys.contains(2));
+    }
+
+    @Test
+    public void test09_put_key_and_contains_it(){
+        final ArrayMap<Integer, String> arrayMap = new ArrayMap<>(Integer::compareTo);
+        arrayMap.put(1, "1");
+        assertTrue(arrayMap.containsKey(1));
+    }
+
+}
