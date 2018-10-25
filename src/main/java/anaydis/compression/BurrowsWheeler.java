c78cc3d701bb37a8b4679c100a4b718290dc6f10
matiasmiodosky <matias.miodosky@ing.austral.edu.ar>
2018-10-25 00:27:13 -0300

    Burrows Wheeler fixed #1

--- src/main/java/anaydis/compression/BurrowsWheeler.java
+++ src/main/java/anaydis/compression/BurrowsWheeler.java
@@ -2,10 +2,9 @@ package anaydis.compression;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.lang.reflect.Array;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.NoSuchElementException;
 
@@ -24,49 +23,46 @@ public class BurrowsWheeler implements Compressor {
 
         for (int i = 0; i < shifting.length; i++) {
             CircleString aShifting = shifting[i];
-            if (aShifting.getStart() ==  1) position =i;
+            if (aShifting.getStart() ==  1 ) position =i;
                     output.write(aShifting.last());
         }
 
-        output.write(position);
+        writeInt(position, output);
+        System.out.println();
     }
 
     //encoding utils
     private CircleString readCircleString(InputStream inputStream) throws IOException {
-       return new CircleString(readString(inputStream));
-    }
-
-    @Override
-    public void decode(@NotNull InputStream input, @NotNull OutputStream output) throws IOException {
-        final byte[] inputArray = streamToArray(input);
-        final byte[] string = Arrays.copyOfRange(inputArray, 0, inputArray.length - 1);
-        final int[] transformVector = getTVector(string);
-        int current = (int) inputArray[inputArray.length - 1];
-        for (int i = 0; i < string.length; i++) {
-            output.write(string[current]);
-            current = transformVector[current];
-        }
-    }
-
-    private byte[] streamToArray(InputStream input) throws IOException {
-        final byte[] array = new byte[input.available()];
-        byte read = (byte) input.read();
-        int i = 0;
-        while (read != -1){
-            array[i++] = read;
-            read = (byte) input.read();
-        }
-        return array;
-    }
-
-    //decoding utils
-    private char[] readString(InputStream inputStream) throws IOException {
         int read = inputStream.read();
         char[] chars = new char[inputStream.available() + 1];
         int i = 0;
         while (read != -1){
             chars[i++] = (char) read;
             read = inputStream.read();
+        }
+       return new CircleString(chars);
+    }
+
+    @Override
+    public void decode(@NotNull InputStream input, @NotNull OutputStream output) throws IOException {
+        final byte[] string = readString(input);
+        final int[] transformVector = getTVector(string);
+        int current = readInt(input);
+        for (int i = 0; i < string.length; i++) {
+            output.write(string[current]);
+            current = transformVector[current];
+        }
+        System.out.println();
+    }
+
+    //decoding utils
+    private byte[] readString(InputStream inputStream) throws IOException {
+        int read;
+        byte[] chars = new byte[inputStream.available() - Integer.BYTES ];
+        int i = 0;
+        while (i < chars.length ){
+            read = inputStream.read();
+            chars[i++] = (byte) read;
         }
         return chars;
     }
@@ -91,5 +87,28 @@ public class BurrowsWheeler implements Compressor {
             }
         }
         throw new NoSuchElementException();
+    }
+
+
+    //bits utils
+    public static void writeInt(int value, OutputStream outputStream) throws IOException {
+        outputStream.write(value >> 24);
+        outputStream.write(value >> 16);
+        outputStream.write(value >> 8);
+        outputStream.write(value);
+        System.out.println();
+    }
+
+    public static int readInt(InputStream inputStream) throws IOException {
+        byte[] bytes = new byte[4];
+        inputStream.read(bytes);
+        return ByteBuffer.wrap(bytes).getInt();
+    }
+
+    public static void main(String[] args) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        writeInt(544, byteArrayOutputStream);
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+        System.out.println(readInt(byteArrayInputStream));
     }
 }
